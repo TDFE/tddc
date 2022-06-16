cefba209e7d612c756925c8a211d87bc18c969d4
周泽飞 <zefei.zhou@tongdun.net>
2022-06-16 12:00:10 +0800

    dumi

--- .umirc.js
+++ .umirc.js
@@ -27,7 +27,9 @@ export default defineConfig({
   title: 'tddc',
   favicon: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
-  outputPath: 'tddc-resource',
+  outputPath: 'lerna-resource',
+  publicPath: process.env.NODE_ENV === 'production' ? '/tddc/' : '/',
+  base:process.env.NODE_ENV === 'production' ? '/tddc/' : '/',
   mode: 'site',
   extraBabelPlugins: [
     [
