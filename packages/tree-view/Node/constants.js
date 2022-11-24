716a0eba7d0376e0a4c6d4c40e04d9df0f1d27cc
jun.zhang <1484287277@qq.com>
2022-11-24 19:34:53 +0800

    fix: 中台1.5节点添加

--- packages/tree-view/Node/constants.js
+++ packages/tree-view/Node/constants.js
@@ -63,6 +63,9 @@ export const NODE_TYPE_MAP = {
   NO_DISTURB: { name: '免打扰', color: '#7ABD87', img: require('./imgs/17.svg') },
   API_SERVICE: { name: '对接服务', color: '#945FB9', img: require('./imgs/18.svg') },
   ETL_HANDLER: { name: 'ETL处理器', color: '#5681B0', img: require('./imgs/19.svg') },
+  INDEX_DATASOURCE: { name: '外数指标', color: '#BC6BC6', img: require('./imgs/22.svg') },
+  INDEX_YUNTU: { name: '图谱指标', color: '#BC6BC6', img: require('./imgs/23.svg') },
+  INDEX_YUNTU_VERSION: { name: '图谱指标', color: '#BC6BC6', img: require('./imgs/23.svg') },
 };
 
 export default constant;
