2cfb94ffcc6586b36f98584714a222589f9d7221
jun.zhang <1484287277@qq.com>
2023-03-08 19:19:30 +0800

    fix: 支持外部数据拼接节点

--- packages/tree-view/src/treeView/constants.js
+++ packages/tree-view/src/treeView/constants.js
@@ -1,3 +1,5 @@
+import { SVGImage } from './utils';
+
 export const RELATION_WIDTH = 120;
 export const COMPONENT_HEIGHT = 40;
 export const COMPONENT_WIDTH = 120;
@@ -27,51 +29,51 @@ let constant = {
 
 // 天策
 export const tiance = {
-  POLICY: { en: 'P', cn: '策', color: '#D97B4E' },
-  POLICY_VERSION: { en: 'P', cn: '策', color: '#D97B4E' },
-  POLICY_PIBLISH_HIS: { en: 'P', cn: '策', color: '#C78F58' },
+  POLICY: { en: 'P', cn: '策', color: '#D97B4E', img: SVGImage('策', '#D97B4E') },
+  POLICY_VERSION: { en: 'P', cn: '策', color: '#D97B4E', img: SVGImage('策', '#D97B4E') },
+  POLICY_PIBLISH_HIS: { en: 'P', cn: '策', color: '#C78F58', img: SVGImage('策', '#D97B4E') },
 
-  DECISION_TOOL: { en: 'D', cn: '决', color: '#357BEB' },
-  DECISION_TOOL_VERSION: { en: 'D', cn: '决', color: '#357BEB' },
+  DECISION_TOOL: { en: 'D', cn: '决', color: '#357BEB', img: SVGImage('决', '#357BEB') },
+  DECISION_TOOL_VERSION: { en: 'D', cn: '决', color: '#357BEB', img: SVGImage('决', '#357BEB') },
 
-  THIRD_SERVICE: { en: 'T', cn: '三', color: '#3BA99E' },
-  THIRD_SERVICE_VERSION: { en: 'T', cn: '三', color: '#3BA99E' },
-  DATASOURCE_SERVICE: { en: 'T', cn: '三', color: '#3BA99E' },
+  THIRD_SERVICE: { en: 'T', cn: '三', color: '#3BA99E', img: SVGImage('三', '#3BA99E') },
+  THIRD_SERVICE_VERSION: { en: 'T', cn: '三', color: '#3BA99E', img: SVGImage('三', '#3BA99E') },
+  DATASOURCE_SERVICE: { en: 'T', cn: '三', color: '#3BA99E', img: SVGImage('三', '#3BA99E') },
 
-  MODEL: { en: 'M', cn: '模', color: '#91B53B' },
-  MODEL_VERSION: { en: 'M', cn: '模', color: '#91B53B' },
+  MODEL: { en: 'M', cn: '模', color: '#91B53B', img: SVGImage('模', '#91B53B') },
+  MODEL_VERSION: { en: 'M', cn: '模', color: '#91B53B', img: SVGImage('模', '#91B53B') },
 
-  RULE: { en: 'R', cn: '规', color: '#126BFB' },
+  RULE: { en: 'R', cn: '规', color: '#126BFB', img: SVGImage('规', '#126BFB') },
 
-  RULE_SET: { en: 'R', cn: '规', color: '#1DA3DA' },
-  RULE_SET_VERSION: { en: 'R', cn: '规', color: '#1DA3DA' },
+  RULE_SET: { en: 'R', cn: '规', color: '#1DA3DA', img: SVGImage('规', '#1DA3DA') },
+  RULE_SET_VERSION: { en: 'R', cn: '规', color: '#1DA3DA', img: SVGImage('规', '#1DA3DA') },
 
-  INDEX_OFFLINE: { en: 'O', cn: '离', color: '#BC6BC6' },
-  INDEX_OFFLINE_VERSION: { en: 'O', cn: '离', color: '#BC6BC6' },
+  INDEX_OFFLINE: { en: 'O', cn: '离', color: '#BC6BC6', img: SVGImage('离', '#BC6BC6') },
+  INDEX_OFFLINE_VERSION: { en: 'O', cn: '离', color: '#BC6BC6', img: SVGImage('离', '#BC6BC6') },
 
-  INDEX_REALTIME: { en: 'R', cn: '实', color: '#0BBBD1' },
-  INDEX_REALTIME_VERSION: { en: 'R', cn: '实', color: '#0BBBD1' },
+  INDEX_REALTIME: { en: 'R', cn: '实', color: '#0BBBD1', img: SVGImage('实', '#0BBBD1') },
+  INDEX_REALTIME_VERSION: { en: 'R', cn: '实', color: '#0BBBD1', img: SVGImage('实', '#0BBBD1') },
 
-  FIELD_SYSTEM: { en: 'S', cn: '系', color: '#6776CF' },
-  FIELD_SCRIPT: { en: 'D', cn: '动', color: '#5B8FF9 ' },
+  FIELD_SYSTEM: { en: 'S', cn: '系', color: '#6776CF', img: SVGImage('系', '#6776CF') },
+  FIELD_SCRIPT: { en: 'D', cn: '动', color: '#5B8FF9 ', img: SVGImage('动', '#5B8FF9') },
 
-  DEAL_TYPE: { en: 'R', cn: '风', color: '#CF6867' },
-  EVENT_TYPE: { en: 'E', cn: '事', color: '#D5A142' },
-  FUNCTION: { en: 'F', cn: '函', color: '#DB6595' },
-  FUNCTION_VERSION: { en: 'F', cn: '函', color: '#DB6595' },
-  NAME_LIST: { en: 'L', cn: '名单集', color: '#E6A133' },
-  FLOW_TEMPLATE: { en: 'P', cn: '流', color: '#72A768' },
-  RULE_IMMUNE: { en: 'R', cn: '规', color: '#565ED5' },
-  NO_DISTURB: { en: 'D', cn: '免', color: '#7ABD87' },
-  API_SERVICE: { en: 'D', cn: '对', color: '#945FB9' },
+  DEAL_TYPE: { en: 'R', cn: '风', color: '#CF6867', img: SVGImage('风', '#CF6867') },
+  EVENT_TYPE: { en: 'E', cn: '事', color: '#D5A142', img: SVGImage('系', '#D5A142') },
+  FUNCTION: { en: 'F', cn: '函', color: '#DB6595', img: SVGImage('函', '#DB6595') },
+  FUNCTION_VERSION: { en: 'F', cn: '函', color: '#DB6595', img: SVGImage('函', '#DB6595') },
+  NAME_LIST: { en: 'L', cn: '名', color: '#E6A133', img: SVGImage('名', '#E6A133') },
+  FLOW_TEMPLATE: { en: 'P', cn: '流', color: '#72A768', img: SVGImage('流', '#72A768') },
+  RULE_IMMUNE: { en: 'R', cn: '规', color: '#565ED5', img: SVGImage('规', '#565ED5') },
+  NO_DISTURB: { en: 'D', cn: '免', color: '#7ABD87', img: SVGImage('免', '#7ABD87') },
+  API_SERVICE: { en: 'D', cn: '对', color: '#945FB9', img: SVGImage('对', '#945FB9') },
 
-  ETL_HANDLER: { en: 'E', cn: 'E', color: '#5681B0' },
-  DATASOURCE_ETL_HANDLER: { en: 'E', cn: 'E', color: '#5681B0' },
+  ETL_HANDLER: { en: 'E', cn: 'E', color: '#5681B0', img: SVGImage('E', '#5681B0') },
+  DATASOURCE_ETL_HANDLER: { en: 'E', cn: 'E', color: '#5681B0', img: SVGImage('E', '#5681B0') },
 
-  INDEX_DATASOURCE: { en: 'E', cn: '外', color: '#BC6BC6' },
-  INDEX_YUNTU: { en: 'G', cn: '图', color: '#BC6BC6' },
-  INDEX_YUNTU_VERSION: { en: 'G', cn: '图', color: '#BC6BC6' },
-  SCORE_CARD: { en: 'S', cn: '评', color: '#DB6595' },
+  INDEX_DATASOURCE: { en: 'E', cn: '外', color: '#BC6BC6', img: SVGImage('外', '#BC6BC6') },
+  INDEX_YUNTU: { en: 'G', cn: '图', color: '#BC6BC6', img: SVGImage('图', '#BC6BC6') },
+  INDEX_YUNTU_VERSION: { en: 'G', cn: '图', color: '#BC6BC6', img: SVGImage('图', '#BC6BC6') },
+  SCORE_CARD: { en: 'S', cn: '评', color: '#DB6595', img: SVGImage('评', '#DB6595') },
 };
 
 // 天座
@@ -82,44 +84,52 @@ export const tianzuo = {
     en: 'E',
     cn: 'E',
     color: '#D5A142',
+    img: SVGImage('E', '#D5A142'),
   },
 
   DATASOURCE_ETL_HANDLER_POST: {
     en: 'P',
     name: '后',
     color: '#3BA99E',
+    img: SVGImage('后', '#3BA99E'),
   },
   DATASOURCE_ETL_HANDLER_PRE: {
     en: 'P',
     name: '前',
     color: '#1DA3DA',
+    img: SVGImage('前', '#1DA3DA'),
   },
   DATASOURCE_CHANNEL_SERVICE: {
     en: 'C',
     name: '调',
     color: '#945FB9',
+    img: SVGImage('调', '#945FB9'),
   },
   DATASOURCE_SERVICE_SHUNT: {
     en: 'S',
     name: '分',
     color: '#6776CF',
+    img: SVGImage('分', '#6776CF'),
   },
   DATASOURCE_SERVICE_EXCEPTION: {
     en: 'A',
-    name: '异常切换接口',
+    name: '异',
     color: '#D97B4E',
+    img: SVGImage('异', '#D97B4E'),
   },
   DATASOURCE_CHANNEL_SERVICE_GROUP: {
     en: 'C',
     name: '调',
     color: '#DB6595',
+    img: SVGImage('调', '#DB6595'),
   },
   EXECUTE_MODE_PARALLEL: {
     en: 'P',
     name: '并',
     color: '#91B53B',
+    img: SVGImage('并', '#91B53B'),
   },
-  EXECUTE_MODE_SERIAL: { en: 'S', name: '串', color: '#72A768' },
+  EXECUTE_MODE_SERIAL: { en: 'S', name: '串', color: '#72A768', img: SVGImage('并', '#91B53B') },
 };
 
 export const NODE_TYPE_MAP = {
