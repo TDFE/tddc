3439d955e445b366c568da28557a5898d23e1917
周泽飞 <zefei.zhou@tongdun.net>
2023-10-31 16:02:20 +0800

    lib-service

--- packages/layout/lib/TNTLayout/SideMenu/index.js
+++ packages/layout/lib/TNTLayout/SideMenu/index.js
@@ -1,22 +1,22 @@
 'use strict';
 
-function _typeof(o) {
+function _typeof(obj) {
   '@babel/helpers - typeof';
   return (
     (_typeof =
       'function' == typeof Symbol && 'symbol' == typeof Symbol.iterator
-        ? function (o) {
-            return typeof o;
+        ? function (obj) {
+            return typeof obj;
           }
-        : function (o) {
-            return o &&
+        : function (obj) {
+            return obj &&
               'function' == typeof Symbol &&
-              o.constructor === Symbol &&
-              o !== Symbol.prototype
+              obj.constructor === Symbol &&
+              obj !== Symbol.prototype
               ? 'symbol'
-              : typeof o;
+              : typeof obj;
           }),
-    _typeof(o)
+    _typeof(obj)
   );
 }
 Object.defineProperty(exports, '__esModule', {
@@ -115,32 +115,39 @@ function _arrayLikeToArray(arr, len) {
   for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i];
   return arr2;
 }
-function _iterableToArrayLimit(r, l) {
-  var t =
-    null == r ? null : ('undefined' != typeof Symbol && r[Symbol.iterator]) || r['@@iterator'];
-  if (null != t) {
-    var e,
-      n,
-      i,
-      u,
-      a = [],
-      f = !0,
-      o = !1;
+function _iterableToArrayLimit(arr, i) {
+  var _i =
+    null == arr
+      ? null
+      : ('undefined' != typeof Symbol && arr[Symbol.iterator]) || arr['@@iterator'];
+  if (null != _i) {
+    var _s,
+      _e,
+      _x,
+      _r,
+      _arr = [],
+      _n = !0,
+      _d = !1;
     try {
-      if (((i = (t = t.call(r)).next), 0 === l)) {
-        if (Object(t) !== t) return;
-        f = !1;
-      } else for (; !(f = (e = i.call(t)).done) && (a.push(e.value), a.length !== l); f = !0);
-    } catch (r) {
-      (o = !0), (n = r);
+      if (((_x = (_i = _i.call(arr)).next), 0 === i)) {
+        if (Object(_i) !== _i) return;
+        _n = !1;
+      } else
+        for (
+          ;
+          !(_n = (_s = _x.call(_i)).done) && (_arr.push(_s.value), _arr.length !== i);
+          _n = !0
+        );
+    } catch (err) {
+      (_d = !0), (_e = err);
     } finally {
       try {
-        if (!f && null != t.return && ((u = t.return()), Object(u) !== u)) return;
+        if (!_n && null != _i.return && ((_r = _i.return()), Object(_r) !== _r)) return;
       } finally {
-        if (o) throw n;
+        if (_d) throw _e;
       }
     }
-    return a;
+    return _arr;
   }
 }
 function _arrayWithHoles(arr) {
