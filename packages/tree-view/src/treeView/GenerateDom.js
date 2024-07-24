eb4a7b2c605ef82d5e84ade905a980aa222106df
popoqaq <1484287277@qq.com>
2024-07-24 10:15:24 +0800

    tree-view更新

---
+++ packages/tree-view/src/treeView/GenerateDom.js
@@ -0,0 +1,89 @@
+import React, { Component, createRef } from 'react';
+import ReactDOM from 'react-dom';
+
+function chunkRender(dataArr, callback) {
+  let delay = 100;
+  let chunkSize = 50; // 设置切割大小
+
+  const chunks = Array(Math.ceil(dataArr.length / chunkSize))
+    .fill()
+    .map((_, index) => index * chunkSize)
+    .map((begin) => dataArr.slice(begin, begin + chunkSize)); // 将原数组 B 切割成若干个小数组
+
+  let currentIndex = 0;
+
+  function renderChunk() {
+    const start = performance.now();
+
+    while (currentIndex < chunks.length && performance.now() - start < 16) {
+      callback(chunks[currentIndex], currentIndex === chunks.length - 1, currentIndex); // 第二个参数为 true 时，表示已经处理完所有小数组
+      currentIndex++;
+    }
+
+    if (currentIndex < chunks.length) {
+      setTimeout(() => {
+        requestAnimationFrame(renderChunk); // 如果还有未处理完的小数组，则继续请求下一帧
+      }, delay);
+    }
+  }
+
+  setTimeout(() => {
+    requestAnimationFrame(renderChunk); // 先让浏览器渲染一次
+  }, 0);
+}
+
+class HugeArray extends Component {
+  constructor(props) {
+    super(props);
+    this.elements = [];
+    this.container = createRef();
+  }
+
+  componentDidMount() {
+    const { doms } = this.props;
+    // 清除上一次渲染的 DOM 元素
+    this.container.current.innerHTML = '';
+
+    chunkRender(doms, this.handleChunk);
+  }
+
+  componentDidUpdate() {
+    const { doms } = this.props;
+    // 清除上一次渲染的 DOM 元素
+    this.container.current.innerHTML = '';
+
+    chunkRender(doms, this.handleChunk);
+  }
+
+  // 暴露给外部的方法和属性
+
+  handleChunk = (chunk, bool, currentIndex) => {
+    let dom = document.createElement('div');
+    // 设置key值
+    dom.setAttribute('key', currentIndex);
+
+    ReactDOM.render(chunk, dom, () => {
+      this.container.current.appendChild(dom);
+    });
+
+    if (bool) {
+      this.props.onFinish && this.props.onFinish();
+    }
+  };
+
+  render() {
+    return (
+      <div
+        ref={this.container}
+        style={{
+          display: 'absolute',
+          minHeight: 100,
+          width: this.props.width,
+          height: this.props.height,
+        }}
+      />
+    );
+  }
+}
+
+export default HugeArray;
--- packages/tree-view/src/treeView/index.js
+++ packages/tree-view/src/treeView/index.js
@@ -1,4 +1,4 @@
-import { get } from 'lodash';
+import { clone, cloneDeep, debounce, get } from 'lodash';
 import ReactDOM from 'react-dom';
 import Base from './Base';
 import constants, {
@@ -13,11 +13,13 @@ import DefaultNode from './DefaultNode';
 import Link from './Link';
 import { collapseTree, expandTree, getTextPixelWith } from './utils';
 import WrapNode from './WrapNode';
+import GenerateDom from './GenerateDom';
 
 class Tree extends Base {
   constructor() {
     super();
 
+    this.key = 'id';
     this.dom = null;
     this.nodeDom = null;
     this.onChange = null;
@@ -37,16 +39,23 @@ class Tree extends Base {
     this.constant.COMPONENT_SPACE_VERTICAL = COMPONENT_SPACE_VERTICAL; // 节点垂直之间的间隙
     this.constant.COMPONENT_SPACE_HORIZONTAL = COMPONENT_SPACE_HORIZONTAL; // 节点水平之间的间隙
     this.constant.ROOT_WIDTH = ROOT_WIDTH; // 根节点宽度
+
+    this.isFirst = true;
+    this.linesAndDoms = [];
+    this.linesAndDomsNums = 0;
+
+    this.unmountBool = false;
   }
 
   init(props) {
-    let { data, options, styleOptions, container } = props;
-    let { initType, onChange, customPosition, fixed, nodeDom, lineType } = options || {};
+    let { data, options, styleOptions, container, key } = props;
+    let { initType, onChange, customPosition, fixed, nodeDom, lineType, onFinish } = options || {};
     let { nodeWidth, nodeHeight, spaceVertical, spaceHorizontal, rootWidth } = styleOptions || {};
 
+    this.key = key || 'id';
     this.initType = initType || false;
     this.nodeDom = nodeDom || DefaultNode || null;
-    this.onChange = onChange;
+    this.onChange = onChange || (() => {});
     this.customPosition = customPosition;
     this.fixed = fixed;
     this.lineType = lineType || 2;
@@ -62,21 +71,31 @@ class Tree extends Base {
     this.constant.COMPONENT_SPACE_HORIZONTAL = spaceHorizontal || COMPONENT_SPACE_HORIZONTAL; // 节点水平之间的间隙
     this.constant.ROOT_WIDTH = rootWidth || ROOT_WIDTH; // 根节点宽度
 
-    // this.data = data || null;
+    this.onFinish = onFinish || (() => {});
+
     this.setData(data || null);
     if (!this.data) return;
-
+    this.isFirst = false;
     this.render();
   }
 
   setData(data) {
+    this.pre_data_key = this.data?.[this.key];
+    this.initDefault();
     this.data = data;
+  }
+
+  initDefault() {
+    this.Nodes = {};
+    this.linesAndDoms = [];
+    this.flattenNodes = [];
+    this.flattenLinks = [];
+    this.hierarchyData = null;
+    this.finalValue = null;
   }
 
   drawNode() {
     const result = [];
-    // 扁平化树
-    this.flattenNodes = this.Nodes?.descendants() || [];
     this.flattenNodes?.forEach((node) => {
       let { data, x, y } = node;
       let { key, nodeName } = data;
@@ -94,16 +113,20 @@ class Tree extends Base {
           null;
         return NodeIns;
       };
-      let Ele = WrapNode(Eledom);
+      if (!node.parent && !node.data.children) {
+        return;
+      }
       result.push(
-        <Ele
-          key={this.getHierarchyId(key, 'root') + x + y}
+        <WrapNode
+          component={(props) => Eledom(props)}
+          unique={x + y + this.data?.[this.key]}
           id={this.getHierarchyId(key, 'root')}
           width={!isExceed && this.constant.COMPONENT_WIDTH}
           minWidth={this.constant.COMPONENT_WIDTH}
           minHeight={this.constant.COMPONENT_HEIGHT}
           x={x}
           y={y}
+          parent={node.parent}
           fixed={this.fixed}
         />,
       );
@@ -112,8 +135,9 @@ class Tree extends Base {
   }
 
   drawLine() {
-    this.flattenLinks = this.Nodes?.links() || [];
     let result = [];
+    let areadyRendered = new Map();
+
     result =
       this?.flattenLinks.map((link) => {
         const { source, target } = link;
@@ -121,7 +145,6 @@ class Tree extends Base {
         const targetKey = target.data.key;
 
         let x = source.y;
-
         let nodeNameWidth = getTextPixelWith(source.data.nodeName || ''); // 组件名称宽度
 
         if (!source.parent) {
@@ -138,12 +161,47 @@ class Tree extends Base {
           }
         }
 
+        let startPath = [x, source.x];
+        let endPath = [target.y, target.x];
+
+        let shuoldRender = [];
+        const start = [startPath[0], startPath[1], (startPath[0] + endPath[0]) / 2, startPath[1]];
+        const control = [
+          (startPath[0] + endPath[0]) / 2,
+          startPath[1],
+          (startPath[0] + endPath[0]) / 2,
+          endPath[1],
+        ];
+        const end = [(startPath[0] + endPath[0]) / 2, endPath[1], endPath[0], endPath[1]];
+
+        let key = start[0] + '-' + start[1];
+        if (areadyRendered.has(key)) {
+          let s3 = areadyRendered.get(key);
+          if (control[3] < s3) {
+            shuoldRender = [...end];
+          } else {
+            let downKey = key + '-down';
+            if (areadyRendered.has(downKey)) {
+              let downMaxY = areadyRendered.get(downKey);
+              shuoldRender = [start[2], downMaxY, ...end];
+              areadyRendered.set(downKey, control[3]);
+            } else {
+              shuoldRender = [...control, ...end];
+              areadyRendered.set(downKey, control[3]);
+            }
+          }
+        } else {
+          shuoldRender = [...start, ...control, ...end];
+
+          areadyRendered.set(key, start[3]);
+        }
+
         let length = source?.data?.children.length || source?.data?._children.length || 0;
 
         return (
           <div
             key={this.getHierarchyId(sourceKey, targetKey) + source.x + source.y}
-            data-key={this.getHierarchyId(sourceKey, targetKey) + source.x + source.y}
+            // data-key={this.getHierarchyId(sourceKey, targetKey) + source.x + source.y}
           >
             <Link
               root={!source.parent}
@@ -157,6 +215,7 @@ class Tree extends Base {
                 x: target.y,
                 y: target.x,
               }}
+              shuoldRender={shuoldRender}
               type={this.lineType}
               linkType={this.linkType}
             />
@@ -210,6 +269,8 @@ class Tree extends Base {
     this.domHeight = domHeight;
 
     this.Nodes = Nodes;
+    this.flattenLinks = this.Nodes?.links() || [];
+    this.flattenNodes = this.Nodes?.descendants() || [];
   }
 
   // 获取id
@@ -271,31 +332,33 @@ class Tree extends Base {
 
   async expand() {
     await this.data.children.forEach(expandTree);
-    await this.initData();
-    await this.render();
+    this.initData(this.data);
+    this.render();
   }
 
   async packUp() {
     await this.data.children.forEach(collapseTree);
-    await this.initData();
-    await this.render();
+    this.initData(this.data);
+    this.render();
   }
 
   render() {
     this.buildPosition(this.hierarchyData);
+
+    this.onChange && this.onChange(this.data);
+    let nodeDoms = this.drawNode();
+    let lineDoms = this.drawLine();
+    this.linesAndDomsNums = nodeDoms.length + lineDoms.length;
+
     ReactDOM.render(
-      <div
-        style={{
-          position: 'relative',
-          zIndex: 9,
-          width: this.domWidth + 100,
-          height: this.domHeight,
-          margin: '80px 0 0 20px',
-        }}
-      >
-        {this.drawNode()}
-        {this.drawLine()}
-      </div>,
+      <GenerateDom
+        doms={nodeDoms.concat(lineDoms)}
+        linesAndDomsNums={this.linesAndDomsNums}
+        onFinish={this.onFinish}
+        onChange={this.onChange}
+        width={this.domWidth}
+        height={this.domHeight}
+      />,
       this.dom,
     );
   }
