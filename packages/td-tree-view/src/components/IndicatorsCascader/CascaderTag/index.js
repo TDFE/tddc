f8293e61f3b8b38d10fdfce99f5e9ceada8fde0e
‘popoqaq’ <jun.zhang002383@tongdun.me>
2024-09-25 19:14:04 +0800

    国际化修改

--- packages/td-tree-view/src/components/IndicatorsCascader/CascaderTag/index.js
+++ packages/td-tree-view/src/components/IndicatorsCascader/CascaderTag/index.js
@@ -6,57 +6,75 @@
  * @LastEditTime: 2022-12-29 19:55:37
  */
 import { Tooltip } from 'tntd';
+import Cookies from 'universal-cookie';
 import './index.less';
 
 const TYPE_MAP = {
   INT: {
     displayName: '整数',
+    enDisplayName: 'Integer',
     color: '#5262C7',
   },
   DOUBLE: {
     displayName: '小数',
+    enDisplayName: 'Float',
     color: '#00D2C2',
   },
   STRING: {
     displayName: '字符',
+    enDisplayName: 'String',
     color: '#826AF9',
   },
   ENUM: {
     displayName: '枚举',
+    enDisplayName: 'Enum',
     color: '#00C5DC',
   },
   BOOLEAN: {
     displayName: '布尔',
+    enDisplayName: 'Boolean',
     color: '#4A9AF7',
   },
   DATETIME: {
     displayName: '日期',
+    enDisplayName: 'Date',
     color: '#826AF9',
   },
   INTEGER: {
     displayName: '整数',
+    enDisplayName: 'Integer',
     color: '#5262C7',
   },
   FLOAT: {
     displayName: '小数',
+    enDisplayName: 'Float',
     color: '#00D2C2',
   },
   BOOL: {
     displayName: '布尔',
+    enDisplayName: 'Boolean',
     color: '#4A9AF7',
   },
   DATE: {
     displayName: '日期',
+    enDisplayName: 'Date',
     color: '#826AF9',
   },
   ARRAY: {
     displayName: '数组',
+    enDisplayName: 'Array',
     color: '#00D2C2',
   },
 };
 
+const cookies = new Cookies();
+export const getLang = () => {
+  return cookies.get('lang') || 'cn';
+};
+
 const Tag = (props) => {
   let { data = {}, showSourceName = true, bool = true, inputValue, ...rest } = props;
+  const lang = getLang();
   let showImg = false;
   let filedType;
   let dataTypeObj = {};
@@ -72,7 +90,9 @@ const Tag = (props) => {
   return (
     <div className="td-tag" {...rest} style={{ lineHeight: '22px' }}>
       {bool && !inputValue && (
-        <sup style={{ color: dataTypeObj.color }}>{dataTypeObj.displayName} </sup>
+        <sup style={{ color: dataTypeObj.color }}>
+          {lang === 'en' ? dataTypeObj.enDisplayName : dataTypeObj.displayName}{' '}
+        </sup>
       )}
       {showImg && (
         <Tooltip title={'导入待提交'} placement="top">
